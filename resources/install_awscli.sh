95fc4d0dad9b7f7a36faf3f5031042aad36cadbd
Ada Lundhe <ada@datavant.com>
2024-02-08 20:56:37 -0600

    AL: add awscli install script

--- resources/install_awscli.sh
+++ resources/install_awscli.sh
@@ -7,14 +7,12 @@ apt upgrade && \
 
 ARCH=$(arch)
 
-if [[ "$ARCH" == "arm64" ]]; then
+if [[ "$ARCH" == "arm64" || "ARCH" == "aarch64" ]]; then
     curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip" && \
-    unzip awscliv2.zip && \
+    unzip -qq -o awscliv2.zip && \
     ./aws/install
 else
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
-    unzip awscliv2.zip && \
+    unzip -qq -o awscliv2.zip && \
     sudo ./aws/install
 fi
-
-rm -rf awscliv2.zip
