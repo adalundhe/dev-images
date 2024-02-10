ba4e27cc57c25c0c9d66a9bbdd95402dc953663c
Ada Lundhe <ada@datavant.com>
2024-02-10 09:12:55 -0600

    AL: fix k9s in newer docker versions

--- resources/install_k9s.sh
+++ resources/install_k9s.sh
@@ -3,12 +3,6 @@
 set -e
 
 USERNAME=${4:-"automatic"}
-
-if [[ ! -e /usr/local/go/bin ]]; then
-    wget https://golang.org/dl/go1.22.0.linux-amd64.tar.gz \
-    && sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
-fi
-
 # Determine the appropriate non-root user
 if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
     USERNAME=""
@@ -26,18 +20,39 @@ elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
     USERNAME=root
 fi
 
-git clone https://github.com/derailed/k9s.git \
+architecture="$(uname -m)"
+case $architecture in
+    x86_64) architecture="amd64";;
+    aarch64 | armv8*) architecture="arm64";;
+    aarch32 | armv7* | armvhf*) architecture="arm";;
+    i?86) architecture="386";;
+    *) echo "(!) Architecture $architecture unsupported"; exit 1 ;;
+esac
+
+if [[ "$architecture" == "aarch64" ]]; then
+    architecture="arm64"
+fi
+
+if [[ ! -e /usr/local/go/bin ]]; then
+
+    wget "https://golang.org/dl/go1.22.0.linux-$architecture.tar.gz" \
+    && sudo tar -C /usr/local -xzf "go1.22.0.linux-$architecture.tar.gz"
+
+    git clone https://github.com/derailed/k9s.git \
     && cd k9s \
     && make build \
     && mv ./execs/k9s /usr/local/bin \
     && cd .. \
     && rm -rf k9s
 
-# If the tar exists we should remove it and the extra installed go.
-if [[ -e go1.22.0.linux-amd64.tar.gz ]]; then
     rm -rf go1.22.0.linux-amd64.tar.gz
     rm -rf /usr/local/go
+else
+    wget "https://github.com/derailed/k9s/releases/download/v0.31.8/k9s_linux_$architecture.deb"
+    dpkg -i "k9s_linux_$architecture.deb" \
+    rm -rf "k9s_linux_$architecture.deb"
 fi
+
 
 rm -rf /root/go
 rm -rf /home/${USERNAME}/go
