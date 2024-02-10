64b27e33b748057e53d37f535d831bd71f3b558a
Ada Lundhe <ada@datavant.com>
2024-02-10 09:40:06 -0600

    AL: fix k9s in newer docker versions

--- resources/install_k9s.sh
+++ resources/install_k9s.sh
@@ -2,24 +2,6 @@
 
 set -e
 
-USERNAME=${4:-"automatic"}
-# Determine the appropriate non-root user
-if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
-    USERNAME=""
-    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-    for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
-        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
-            USERNAME=${CURRENT_USER}
-            break
-        fi
-    done
-    if [ "${USERNAME}" = "" ]; then
-        USERNAME=root
-    fi
-elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
-    USERNAME=root
-fi
-
 architecture="$(uname -m)"
 case $architecture in
     x86_64) architecture="amd64";;
@@ -33,26 +15,6 @@ if [[ "$architecture" == "aarch64" ]]; then
     architecture="arm64"
 fi
 
-if [[ ! -e /usr/local/go/bin ]]; then
-
-    wget "https://golang.org/dl/go1.22.0.linux-$architecture.tar.gz" \
-    && sudo tar -C /usr/local -xzf "go1.22.0.linux-$architecture.tar.gz"
-
-    git clone https://github.com/derailed/k9s.git \
-    && cd k9s \
-    && make build \
-    && mv ./execs/k9s /usr/local/bin \
-    && cd .. \
-    && rm -rf k9s
-
-    rm -rf go1.22.0.linux-amd64.tar.gz
-    rm -rf /usr/local/go
-else
-    wget "https://github.com/derailed/k9s/releases/download/v0.31.8/k9s_linux_$architecture.deb"
-    dpkg -i "k9s_linux_$architecture.deb"
-    rm -rf "k9s_linux_$architecture.deb"
-fi
-
-
-rm -rf /root/go
-rm -rf /home/${USERNAME}/go
+wget "https://github.com/derailed/k9s/releases/download/v0.31.8/k9s_linux_$architecture.deb"
+dpkg -i "k9s_linux_$architecture.deb"
+rm -rf "k9s_linux_$architecture.deb"
