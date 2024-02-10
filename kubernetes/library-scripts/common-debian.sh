244fe86d766c995050995651ee4fd503fba45bd9
Ada Lündhé <ada@datavant.com>
2024-02-09 19:49:32 -0600

    Update common-debian.sh

--- kubernetes/library-scripts/common-debian.sh
+++ kubernetes/library-scripts/common-debian.sh
@@ -44,6 +44,7 @@ apt_get_update_if_needed()
 {
     if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
         echo "Running apt-get update..."
+        apt-get clean
         apt-get update
     else
         echo "Skipping apt-get update."
