8ef429c876f2fd07244d9f573a2ca10ebd140bb6
Ada Lundhe <ada@datavant.com>
2024-02-09 23:42:52 -0600

    Remove Kubernetes SHIM image

--- resources/install_k9s.sh
+++ resources/install_k9s.sh
@@ -4,7 +4,8 @@ set -e
 
 if [[ ! -e /usr/local/go/bin ]]; then
     wget https://golang.org/dl/go1.22.0.linux-amd64.tar.gz \
-    && sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+    && sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz && \
+
 fi
 
 git clone https://github.com/derailed/k9s.git \
@@ -12,7 +13,8 @@ git clone https://github.com/derailed/k9s.git \
     && make build \
     && mv ./execs/k9s /usr/local/bin
 
-if [[ ! -e go1.22.0.linux-amd64.tar.gz ]]; then
+# If the tar exists we should remove it and the extra installed go.
+if [[ -e go1.22.0.linux-amd64.tar.gz ]]; then
     rm -rf go1.22.0.linux-amd64.tar.gz
     rm -rf /usr/local/go
 fi
