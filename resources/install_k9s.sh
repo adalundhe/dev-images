eea9cc85198375b3b4b3a79eb06675da240fa865
Ada Lundhe <ada@datavant.com>
2024-02-10 11:04:28 -0600

    AL: update install script to pull latest version

--- resources/install_k9s.sh
+++ resources/install_k9s.sh
@@ -1,20 +1,23 @@
-#!/bin/bash -i
+#!/usr/bin/env bash
 
 set -e
 
 architecture="$(uname -m)"
 case $architecture in
     x86_64) architecture="amd64";;
-    aarch64 | armv8*) architecture="arm64";;
+    aarch64 | arm64 | armv8*) architecture="arm64";;
     aarch32 | armv7* | armvhf*) architecture="arm";;
     i?86) architecture="386";;
     *) echo "(!) Architecture $architecture unsupported"; exit 1 ;;
 esac
 
-if [[ "$architecture" == "aarch64" ]]; then
-    architecture="arm64"
-fi
+curl -s https://api.github.com/repos/derailed/k9s/releases/latest \
+| grep "k9s_linux_$architecture.deb" \
+| cut -d : -f 3 \
+| tr -d \" \
+| tr -d '\n' \
+| wget "https:$(cat -)"
 
-wget "https://github.com/derailed/k9s/releases/download/v0.31.8/k9s_linux_$architecture.deb"
+
 dpkg -i "k9s_linux_$architecture.deb"
 rm -rf "k9s_linux_$architecture.deb"
